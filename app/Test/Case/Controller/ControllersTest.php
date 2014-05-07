1fd1fe9d9e7094b847be118c2dbd0a91002792e7
tranfuga25s <tranfuga25s@gmail.com>
2014-05-07 17:42:54 -0300

    Eliminado test sin contenido

--- app/Test/Case/Controller/ControllersTest.php
+++ app/Test/Case/Controller/ControllersTest.php
@@ -12,7 +12,7 @@ class ControllersTests extends PHPUnit_Framework_TestSuite {
 	public static function suite() {
    	    $suite = new PHPUnit_Framework_TestSuite('Test de controladores');
             $path = APP_TEST_CASES . DS . 'Controller' . DS;
-            $suite->addTestFile( $path.'CtactesControllerTest.php' );
+            //$suite->addTestFile( $path.'CtactesControllerTest.php' );
             $suite->addTestFile( $path.'NoticiasControllerTest.php' );
             $suite->addTestFile( $path.'UsuariosControllerTest.php' );
             return $suite;
