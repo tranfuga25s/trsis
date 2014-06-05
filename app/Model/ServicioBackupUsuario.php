8bfbdf530687b7ce34a8d8f6ff00ac940699b9e4
tranfuga25s <tranfuga25s@gmail.com>
2014-06-04 23:40:49 -0300

    Mejorados fixtures y codigo de servicio backup usuario.

--- app/Model/ServicioBackupUsuario.php
+++ app/Model/ServicioBackupUsuario.php
@@ -1,65 +1,84 @@
 <?php
+
 App::uses('AppModel', 'Model');
+
 /**
  * ServicioBackupUsuario Model
  *
  */
 class ServicioBackupUsuario extends AppModel {
-/**
- * Use table
- *
- * @var mixed False or table name
- */
-	public $useTable = 'servicio_backup_usuario';
-/**
- * Primary key field
- *
- * @var string
- */
-	public $primaryKey = 'id_servicio_backup';
 
-	public $belongsTo =array(
-				'ServicioBackup' => array(
-						'className' => 'ServicioBackup',
-						'foreignKey' => 'id_servicio_backup')
-				);
+    /**
+     * Use table
+     *
+     * @var mixed False or table name
+     */
+    public $useTable = 'servicio_backup_usuario';
 
-	public function buscarCodigo( $id_usuario ) {
-		$d = $this->find( 'first', array( 'conditions' => array( 'id_usuario' => $id_usuario ), 'recursive' => -1, 'fields' => array( 'codigo' ) ) );
-		return $d['ServicioBackupUsuario']['codigo'];
-	}
+    /**
+     * Primary key field
+     *
+     * @var string
+     */
+    public $primaryKey = 'id_servicio_backup';
+    public $belongsTo = array(
+        'ServicioBackup' => array(
+            'className' => 'ServicioBackup',
+            'foreignKey' => 'id_servicio_backup')
+    );
 
-	public function buscarIdServicioBackup( $id_usuario ) {
-		$d = $this->find( 'first', array( 'conditions' => array( 'ServicioBackupUsuario.id_usuario' => $id_usuario ),
-										  'fields' => array( 'id_servicio_backup' ),
-										  'recursive' => 1 ) );
-		return $d['ServicioBackupUsuario']['id_servicio_backup'];
-	}
+    /**
+     * Devuelve el codigo para un ID de usuario predeterminado
+     * @param integer $id_usuario
+     * @return string
+     */
+    public function buscarCodigo( $id_usuario = null ) {
+        if( is_null( $id_usuario ) || intval( $id_usuario <= 0 ) ) {
+            return false;
+        }
+        $d = $this->find( 'first', array( 
+            'conditions' => array(
+                'id_usuario' => $id_usuario
+            ), 
+            'recursive' => -1, 
+            'fields' => array( 'codigo' ) )
+        );
+        if( count( $d ) > 0 && array_key_exists( $this->alias, $d ) ) {
+            return $d['ServicioBackupUsuario']['codigo'];
+        }
+        return false;
+    }
 
-	public function verificarRelacionUsuario( $id_usuario, $id_servicio_backup ) {
-		if( $id_usuario == null || $id_servicio_backup == null ) {
-			return true;
-		}
+    public function buscarIdServicioBackup($id_usuario) {
+        $d = $this->find('first', array('conditions' => array('ServicioBackupUsuario.id_usuario' => $id_usuario),
+            'fields' => array('id_servicio_backup'),
+            'recursive' => 1));
+        return $d['ServicioBackupUsuario']['id_servicio_backup'];
+    }
 
-		$d = $this->find( 'count', array(
-			'conditions' => array(
-				'id_usuario' => $id_usuario,
-				'id_servicio_backup' => $id_servicio_backup
-			),
-			'recursive' => -1
-		));
-		if( $d > 0 ) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public function verificarRelacionUsuario($id_usuario, $id_servicio_backup) {
+        if ($id_usuario == null || $id_servicio_backup == null) {
+            return true;
+        }
 
-	public function actualizarUso( $num_cliente, $cantidad, $tam, $id_servicio ) {
-		$data = $this->find( 'first', array( 'conditions' => array( 'id_usuario' => $num_cliente, 'id_servicio_backup' => $id_servicio ), 'recursive' => -1 ) );
-		$this->updateAll( array( 'cantidad' => $data['ServicioBackupUsuario']['cantidad'] + $cantidad,
-								 'espacio' => $data['ServicioBackupUsuario']['espacio'] + $tam  ),
-						  array( '`ServicioBackupUsuario`.`id_usuario`' => $num_cliente, '`ServicioBackupUsuario`.`id_servicio_backup`' => $id_servicio ) ) ;
-	}
+        $d = $this->find('count', array(
+            'conditions' => array(
+                'id_usuario' => $id_usuario,
+                'id_servicio_backup' => $id_servicio_backup
+            ),
+            'recursive' => -1
+        ));
+        if ($d > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function actualizarUso($num_cliente, $cantidad, $tam, $id_servicio) {
+        $data = $this->find('first', array('conditions' => array('id_usuario' => $num_cliente, 'id_servicio_backup' => $id_servicio), 'recursive' => -1));
+        $this->updateAll(array('cantidad' => $data['ServicioBackupUsuario']['cantidad'] + $cantidad,
+            'espacio' => $data['ServicioBackupUsuario']['espacio'] + $tam), array('`ServicioBackupUsuario`.`id_usuario`' => $num_cliente, '`ServicioBackupUsuario`.`id_servicio_backup`' => $id_servicio));
+    }
 
 }
--- app/Test/Fixture/BackupFixture.php
+++ app/Test/Fixture/BackupFixture.php
@@ -1,51 +1,51 @@
 <?php
-/* Backup Fixture generated on: 2012-05-23 21:52:53 : 1337820773 */
-
 /**
  * BackupFixture
  *
  */
 class BackupFixture extends CakeTestFixture {
-/**
- * Table name
- *
- * @var string
- */
-	public $table = 'backup';
 
-/**
- * Fields
- *
- * @var array
- */
-	public $fields = array(
-		'id_backup' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20, 'key' => 'primary', 'collate' => NULL, 'comment' => ''),
-		'servicio_backup_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20, 'key' => 'index', 'collate' => NULL, 'comment' => ''),
-		'usuario_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20, 'key' => 'index', 'collate' => NULL, 'comment' => ''),
-		'fecha' => array('type' => 'datetime', 'null' => false, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
-		'tamano' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 20, 'collate' => NULL, 'comment' => ''),
-		'archivo_db' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_spanish_ci', 'comment' => '', 'charset' => 'utf8'),
-		'archivo_est' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_spanish_ci', 'comment' => '', 'charset' => 'utf8'),
-		'archivo_pref' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_spanish_ci', 'comment' => '', 'charset' => 'utf8'),
-		'indexes' => array('PRIMARY' => array('column' => 'id_backup', 'unique' => 1), 'servicio_backup_id' => array('column' => 'servicio_backup_id', 'unique' => 0), 'usuario_id' => array('column' => 'usuario_id', 'unique' => 0)),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_spanish_ci', 'engine' => 'InnoDB')
-	);
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    public $table = 'backup';
 
-/**
- * Records
- *
- * @var array
- */
-	public $records = array(
-		array(
-			'id_backup' => 1,
-			'servicio_backup_id' => 1,
-			'usuario_id' => 1,
-			'fecha' => '2012-05-23 21:52:53',
-			'tamano' => 1,
-			'archivo_db' => 'Lorem ipsum dolor sit amet',
-			'archivo_est' => 'Lorem ipsum dolor sit amet',
-			'archivo_pref' => 'Lorem ipsum dolor sit amet'
-		),
-	);
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    public $fields = array(
+        'id_backup' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20, 'key' => 'primary', 'collate' => NULL, 'comment' => ''),
+        'servicio_backup_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20, 'key' => 'index', 'collate' => NULL, 'comment' => ''),
+        'usuario_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20, 'key' => 'index', 'collate' => NULL, 'comment' => ''),
+        'fecha' => array('type' => 'datetime', 'null' => false, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
+        'tamano' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 20, 'collate' => NULL, 'comment' => ''),
+        'archivo_db' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_spanish_ci', 'comment' => '', 'charset' => 'utf8'),
+        'archivo_est' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_spanish_ci', 'comment' => '', 'charset' => 'utf8'),
+        'archivo_pref' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_spanish_ci', 'comment' => '', 'charset' => 'utf8'),
+        'indexes' => array('PRIMARY' => array('column' => 'id_backup', 'unique' => 1), 'servicio_backup_id' => array('column' => 'servicio_backup_id', 'unique' => 0), 'usuario_id' => array('column' => 'usuario_id', 'unique' => 0)),
+        'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_spanish_ci', 'engine' => 'InnoDB')
+    );
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = array(
+        array(
+            'id_backup' => 1,
+            'servicio_backup_id' => 1,
+            'usuario_id' => 1,
+            'fecha' => '2012-05-23 21:52:53',
+            'tamano' => 1,
+            'archivo_db' => 'Lorem ipsum dolor sit amet',
+            'archivo_est' => 'Lorem ipsum dolor sit amet',
+            'archivo_pref' => 'Lorem ipsum dolor sit amet'
+        ),
+    );
+
 }
